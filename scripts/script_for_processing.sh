89decb51b35245ed8ca3695bc24a3bb2e3b38ed1
JforPlay <starpic414@gmail.com>
2025-10-18 18:59:49 -0400

    validated script / add data for aircraft

--- scripts/script_for_processing.sh
+++ scripts/script_for_processing.sh
@@ -12,6 +12,8 @@ mkdir output
 mkdir output/sim
 mkdir output/misc
 mkdir output/chat-viewer
+mkdir output/skin
+mkdir output/story-viewer
 
 echo processing skindata
 python skin_data_process.py
@@ -19,15 +21,15 @@ python skin_data_process.py
 echo processing story data
 wget https://raw.githubusercontent.com/AzurLaneTools/AzurLaneData/refs/heads/main/KR/GameCfg/dungeon.json
 wget https://raw.githubusercontent.com/AzurLaneTools/AzurLaneData/refs/heads/main/KR/GameCfg/story.json
-python story_data_process.py # creates shipgirl_data.json
+python story_main_data_process.py # creates shipgirl_data.json
 
 echo processing chat-viewer data
 python juustagram_process.py
 python chat_viewer_process.py # also creates ship_group_data with fleet tech
 
 echo processing worlddata
-# python world_story.py # uses shipgirl_data.json created by story_data.py
-# python world_file_collection.py
+# python story_world_data_process.py # uses shipgirl_data.json created by story_data_process.py
+# python story_world_file_collection.py
 
 echo processing barrage data
 wget -O ./output/sim/weapon_property.json https://raw.githubusercontent.com/AzurLaneTools/AzurLaneData/refs/heads/main/KR/sharecfgdata/weapon_property.json
@@ -35,6 +37,7 @@ wget -O ./output/sim/barrage_template.json https://raw.githubusercontent.com/Azu
 wget -O ./output/sim/bullet_template.json https://raw.githubusercontent.com/AzurLaneTools/AzurLaneData/refs/heads/main/KR/sharecfgdata/bullet_template.json
 wget -O ./output/sim/skill_data_template.json https://raw.githubusercontent.com/AzurLaneTools/AzurLaneData/refs/heads/main/KR/ShareCfg/skill_data_template.json
 wget -O ./output/sim/transform_data_template.json https://raw.githubusercontent.com/AzurLaneTools/AzurLaneData/refs/heads/main/KR/ShareCfg/transform_data_template.json
+wget -O ./output/sim/aircraft_template.json https://raw.githubusercontent.com/AzurLaneTools/AzurLaneData/refs/heads/main/KR/sharecfgdata/aircraft_template.json
 wget -O ./output/skill_icon_mapping.json https://raw.githubusercontent.com/Fernando2603/AzurLane/refs/heads/main/skill_icon.json
 python ship_info_process.py
 python sim_weapon_preprocess.py
@@ -43,6 +46,7 @@ python sim_weapon_process.py
 echo others
 python misc_bgm_process.py
 python ship_const_time_process.py
+# python orbit_process.py
 
 echo cleanup files not needed for the final output
 rm story.json dungeon.json
